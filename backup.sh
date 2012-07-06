d8efee41a151c9a34c6da9dc48f7185e9fdcdc5e
Oshan Rube <oshanrube@gmail.com>
2012-07-06 22:42:12 +0530

    with backup.sh compleate

--- backup.sh
+++ backup.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
+DBuser=""
+DBpass=""
+LocalServerPath="/var/www/html/"
+
+echo "removing backup files older than 60 days"
+find ~/backups/ -type f -mtime +60  -exec rm {} \;
 mkdir -p ~/backups
 echo Backup Started `date` >> ~/backups/backuplog
 mkdir ~/backups/`date +%Y%m%d`
-tar -czf ~/backups/`date +%Y%m%d`/data.tar.gz /var/www/html
-mysqldump --user=root --password=oshan1991 --all-databases --lock-tables=false > ~/backups/`date +%Y%m%d`/database.sql
+tar -czf ~/backups/`date +%Y%m%d`/data.tar.gz $LocalServerPath
+mysqldump --user=$DBuser --password=$DBpass --all-databases --lock-tables=false > ~/backups/`date +%Y%m%d`/database.sql
 echo Backup Completed `date` >> ~/backups/backuplog
